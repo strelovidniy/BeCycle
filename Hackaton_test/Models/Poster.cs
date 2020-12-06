990b29ed4898cbb38112035bd5198c97c53a487a
NazariyChetvertukha <carrrson2207@gmail.com>
2020-12-07 00:07:09 +0200

    Updated Poster.cs

--- Hackaton_test/Models/Poster.cs
+++ Hackaton_test/Models/Poster.cs
@@ -1,7 +1,15 @@
-﻿namespace Hackaton_test.Models
+﻿using System;
+
+namespace Hackaton_test.Models
 {
     public class Poster
     {
-        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description {  get; set; }
+        public User User { get; set; }
+        public uint UserId { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime PublicationDate { get; set; }
     }
 }
