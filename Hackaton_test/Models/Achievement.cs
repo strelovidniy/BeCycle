e154c98b8417620c1f6f835ff3cdc839ba46236f
Remok777 <roman.sadokha01@gmail.com>
2020-12-07 00:25:23 +0200

    Installed Google APIs

--- Hackaton_test/Models/Achievement.cs
+++ Hackaton_test/Models/Achievement.cs
@@ -5,5 +5,6 @@ namespace Hackaton_test.Models
         public uint AchieveId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        
     }
 }
