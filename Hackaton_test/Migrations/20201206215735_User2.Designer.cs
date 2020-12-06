42c6f1e22561d8f65321f7709a640a329824c43e
Roman Danylevych <roma.dan2001@gmail.com>
2020-12-06 23:58:35 +0200

    Updated User.cs.

--- Hackaton_test/Migrations/20201206215735_User2.Designer.cs
+++ Hackaton_test/Migrations/20201206215735_User2.Designer.cs
@@ -3,14 +3,16 @@ using Hackaton_test.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Hackaton_test.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20201206215735_User2")]
+    partial class User2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Hackaton_test/Models/User.cs
+++ Hackaton_test/Models/User.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Hackaton_test.Models
 {
+    [Table("User")]
     public class User
     {
-        public uint Id { get; set; }
+        [Column("UserId")] public uint Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
         public string LastName { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
         public uint Age { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
         public string Email { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
         public string PhoneNumber { get; set; }
     }
 }
