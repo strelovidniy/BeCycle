915d1010ee8e614b3b8c7e30835ffaf7c0a0bd8b
Roman Danylevych <roma.dan2001@gmail.com>
2020-12-06 23:29:46 +0200

    Added AppContext.

---
+++ Hackaton_test/Models/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hackaton_test.Models
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
