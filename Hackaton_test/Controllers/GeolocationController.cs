567b5e1d541d15819c5a9a03d40d20a36c85ef35
NazariyChetvertukha <carrrson2207@gmail.com>
2020-12-08 21:55:04 +0200

    Added many geo markers

---
+++ Hackaton_test/Controllers/GeolocationController.cs
@@ -0,0 +1,15 @@
+﻿using Hackaton_test.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hackaton_test.Controllers
+{
+    public class GeolocationController:Controller
+    {
+        GeolocationService geoService = new GeolocationService();
+        public IActionResult Index()
+        {
+            ViewBag.Locations = geoService.geolocationList;
+            return View();
+        }
+    }
+}
