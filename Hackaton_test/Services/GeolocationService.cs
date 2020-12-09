7f5f5c9482bdc6d02bc17166b8e8b39c1a5a2b82
NazariyChetvertukha <carrrson2207@gmail.com>
2020-12-09 02:06:43 +0200

    Replace Geolocation Service

---
+++ Hackaton_test/Services/GeolocationService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Hackaton_test.Services
+{
+    public class GeolocationService:IGeolocationService
+    {
+        private List<string> geolocations = new List<string>()
+        {   "49.848540,24.040773",
+            "49.844123,24.055628",
+            "49.852610,23.994028",
+            "49.813343,24.154294",
+            "49.797176,23.901201",
+            "49.812737,23.893313"
+        };
+        public List<string> GetGeolocations()
+        {
+            return geolocations;
+        }
+
+        public void SetGeolocation(string geolocation)
+        {
+            geolocations.Add(geolocation);
+        }
+        public void RemoveGeolocation(string geolocation)
+        {
+            geolocations.Remove(geolocation);
+        }
+    }
+}
---
+++ Hackaton_test/Services/IGeolocationService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Hackaton_test.Services
+{
+    public interface IGeolocationService
+    {
+        void SetGeolocation(string geolocation);
+        List<string> GetGeolocations();
+        void RemoveGeolocation(string geolocation);
+    }
+}
