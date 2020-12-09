7b236bc8371c3cc1b39807431b51970073cf2c38
Remok777 <roman.sadokha01@gmail.com>
2020-12-09 02:32:37 +0200

    Refactored

--- Hackaton_test/Controllers/PosterCreationController.cs
+++ Hackaton_test/Controllers/PosterCreationController.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using Hackaton_test.Models;
+using Hackaton_test.Controllers;
 
 namespace Hackaton_test.Controllers
 {
     public class PosterCreationController : Controller
     {
         // GET
+       
         public IActionResult Index()
         {
-            return View();
+            Poster poster = new Poster();
+            return View(poster);
         }
     }
 }
