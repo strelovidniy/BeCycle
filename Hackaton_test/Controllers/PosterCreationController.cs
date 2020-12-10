dd0a9b920d384b448570278196a93cb1d66edd99
Roman Danylevych <roma.dan2001@gmail.com>
2020-12-10 23:18:19 +0200

    Refactored.

--- Hackaton_test/Controllers/PosterCreationController.cs
+++ Hackaton_test/Controllers/PosterCreationController.cs
@@ -4,26 +4,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Hackaton_test.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using System.Collections.Generic;
 using System.Security.Claims;
-using Microsoft.AspNetCore.Identity;
 
 namespace Hackaton_test.Controllers
 {
     [Authorize]
     public class PosterCreationController : Controller
     {
-        // GET
         [HttpGet]
         public IActionResult Index()
         {
             var claimsData = ((ClaimsIdentity)HttpContext.User.Identity)?.Claims;
+
             var claimsDictionary = claimsData?
                 .ToDictionary(key => key.Type.Split('/',
                         StringSplitOptions.RemoveEmptyEntries).TakeLast(1).FirstOrDefault(),
                     value => value.Value);
+
             ViewData["UserEmail"] = claimsDictionary?["emailaddress"];
             ViewData["UserName"] = claimsDictionary?["givenname"];
             ViewData["UserSurname"] = claimsDictionary?["surname"];
@@ -40,18 +37,26 @@ namespace Hackaton_test.Controllers
                     .ToDictionary(key => key.Type.Split('/', 
                             StringSplitOptions.RemoveEmptyEntries).TakeLast(1).FirstOrDefault(),
                         value => value.Value);
-                string email = claimsDictionary?["emailaddress"];
-                User currentUser = db.Users.FirstOrDefault(u => u.Email == email);
+
+                var email = claimsDictionary?["emailaddress"];
+                var currentUser = db.Users.FirstOrDefault(u => u.Email == email);
+
                 ViewData["UserName"] = currentUser?.FirstName;
                 ViewData["UserSurname"] = currentUser?.LastName;
+
                 poster.PublicationDate = DateTime.Now;
+                
                 if (currentUser != null)
                 {
                     poster.AuthorId = currentUser.UserId;
-                    EntityEntry<Poster> enPoster = await db.Posters.AddAsync(poster);
+                    
+                    var enPoster = await db.Posters.AddAsync(poster);
+                    
                     await db.SaveChangesAsync(true);
+                
                     return RedirectToAction("Index", "Poster", new {id = enPoster.Entity.PosterId });
                 }
+                
                 return RedirectToRoute("~account/google-signin");
             }
         }
